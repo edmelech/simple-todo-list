3d128c65ffc0f777f1f83dea3dfe448f19bafd54
Ed Melech <mr.melech@gmail.com>
2021-10-19 14:36:39 +0100

    add an addItem function to your componenet witha  value parameter

--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ function App() {
   const [items, setItems] = useState(['Learn React', 'Play Guitar', 'Party'])
   const [newItem, setNewItem] = useState('')
 
+  const addItem = useCallback((item) => {
+    setItems(currentItems => [...currentItems, item])
+  }, [setItems])
+
   return (
    <div className="App">
      <h1>Cyrus' Todo list</h1>
      <ul>
        {
-        items.map((item, index) => 
-          <li>{ item }</li>
-        )   
-      }
+          items.map((item, index) => 
+            <li>{ item }</li>
+          )   
+        }
 
-      <li>
-        <input 
-          type="text"
-          value="{newItem}"
-          onChange={(event) = setNewItem(event.target.value)} />
-      </li>
+        <li>
+          <input 
+            type="text"
+            value="{newItem}"
+            onChange={(event) = setNewItem(event.target.value)} 
+          />
+
+          <button onClick={() => addItem(newItem)}>
+            Add "{ newItem }"
+          </button>
+        </li>
      </ul>
    </div>
   );
