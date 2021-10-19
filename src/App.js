a41b7625e91dfd1ea439add2c4adc507dd27f095
Ed Melech <mr.melech@gmail.com>
2021-10-19 14:00:06 +0100

    create a list with useState and render it as a li

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [items, setItems] = useState(['Learn React, Play Guitar, Party'])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <h1>Cyrus' Todo list</h1>
+     <ul>
+       {
+        items.map((item, index) => 
+          <li>{ item }</li>
+        )   
+      }
+     </ul>
+   </div>
   );
 }
 
